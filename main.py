c8f72e73d266a321a15bbd7dd7e6d479607f40ac
Gabriel Pery <56797835+Xephire@users.noreply.github.com>
2021-09-15 08:35:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+from discord_webhook import DiscordWebhook, DiscordEmbed
+from bs4 import BeautifulSoup
+from keep_alive import keep_alive
+import datetime
+import requests
+
+
+PARSER = "html.parser"
+
+def get_image(url) -> str:
+    content = requests.get(url, PARSER)
+    soup = BeautifulSoup(content.text, features=PARSER)
+
+    img = soup.find("img", attrs={"id": "previewImageMain", "class": "workshopItemPreviewImageMain"})
+    if img is None:
+        img = soup.find("img", attrs={"id": "previewImage", "class": "workshopItemPreviewImageEnlargeable"})
+
+    return img["src"]
+
+def get_title(url) -> str:
+    content = requests.get(url, PARSER)
+    soup = BeautifulSoup(content.text, features=PARSER)
+    title = soup.find("div", attrs={"class": "workshopItemTitle"})
+    return title.text
+
+def get_name(url) -> str:
+    content = requests.get(url, PARSER)
+    soup = BeautifulSoup(content.text, features=PARSER)
+    name = soup.find("div", attrs={"class": "friendBlockContent"})
+    return name.text.strip().split('\n')[0]
+
+def send_wallpaper(wallpaper_url) -> None:
+    url = "https://discord.com/api/webhooks/887076175594278963/24OqyswMZZ6dPhheMhxg_cmw1VNvrmoPjN2qtbGqfDUnLs4SRTbt6-D-AamjaI6j73Hq"
+    get_image(wallpaper_url)
+
+    webhook = DiscordWebhook(url=url)
+    embed = DiscordEmbed(title="Wallpaper of the day", description=f"**{get_title(wallpaper_url)}** - **{get_name(wallpaper_url)}**", color="A020F0")
+    embed.set_url(url=wallpaper_url)
+    embed.set_image(url=get_image(wallpaper_url))
+    webhook.add_embed(embed)
+    webhook.execute()
+
+def choose_wallpaper() -> str:
+    url = "https://steamcommunity.com/workshop/browse/?appid=431960&searchtext=&childpublishedfileid=0&browsesort=trend&section=readytouseitems&requiredtags%5B%5D=Approved&excludedtags%5B%5D=Anime"
+    content = requests.get(url, PARSER)
+    soup = BeautifulSoup(content.text, features=PARSER)
+
+    item = soup.find("a", attrs={"class": "ugc"})
+    return(item["href"])
+
+
+def scheduler():
+    print("Scheduler Running")
+    run = True
+    while True:
+        if datetime.datetime.now().hour == 6 and run:
+            send_wallpaper(wallpaper_url=choose_wallpaper())
+            print(f"Wallpaper successfully sent at {datetime.datetime.now()}")
+            run = False
+        elif datetime.datetime.now().hour != 6:
+            run = True
+
+if __name__ == "__main__":
+    keep_alive()
+    scheduler()
